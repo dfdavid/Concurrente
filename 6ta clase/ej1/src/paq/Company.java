1e3cfc6d1c5fe3aa336d2adbad7ae3de76256ff2
dfdavid <david.do.it@gmail.com>
2017-04-30 23:48:34 -0300

    add c6

---
+++ 6ta clase/ej1/src/paq/Company.java
@@ -0,0 +1,28 @@
+package paq;
+
+/**
+ * Created by iv on 18/04/17.
+ */
+public class Company implements Runnable {
+
+    //private static final int I_END = 100;
+    //private static final double DOUBLE_AMOUNT_TO_ADD = 1_000D;
+
+    private Account account;
+
+    public Company(Account account) {
+        this.account = account;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Runnable#run()
+     */
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            this.account.addAmount(1000);
+        }
+
+    }
+
+}
